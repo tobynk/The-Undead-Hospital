bae93d995401e64ec8306f361663bb81c0138e30
tobynk <122053994+tobynk@users.noreply.github.com>
2024-02-29 14:55:55 -0800

    Player movements added

---
+++ Player.gd
@@ -0,0 +1,16 @@
+extends Area2D
+@export var speed = 400 
+
+func _process(delta):
+	var velocity = Vector2.ZERO 
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
