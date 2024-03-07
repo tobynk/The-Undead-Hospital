ebded829763ee1c9944c4a1c0defa87db703ffec
tobynk <122053994+tobynk@users.noreply.github.com>
2024-03-06 16:18:37 -0800

    redid interactable

--- interactable.gd
+++ interactable.gd
@@ -1,13 +1,20 @@
 extends Area2D
-@onready var interaction_area = $InteractionArea
 @onready var label = $Label
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	label.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+
+func _on_area_entered(area):
+	label.show()
+
+
+func _on_area_exited(area):
+	label.hide()
