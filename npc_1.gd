14153910c08b5c58517e777dc914958f0aa613ab
tobynk <122053994+tobynk@users.noreply.github.com>
2024-03-07 11:30:20 -0800

    fix

--- npc_1.gd
+++ npc_1.gd
@@ -1,13 +1,22 @@
 extends Area2D
-@onready var interaction_area = $InteractionArea
 @onready var label = $Label
+var interactionarea = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
+	label.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if interactionarea and Input.is_action_just_pressed("interacted"):
+		print("e is pressed")
+	
+func _on_area_entered(area):
+	label.show()
+	interactionarea = true
+
+
+func _on_area_exited(area):
+	label.hide()
+	interactionarea = false
 
