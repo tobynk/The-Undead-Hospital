ee7e0bef4764106feb0644ce0a808bf6036f4892
tobynk <122053994+tobynk@users.noreply.github.com>
2024-05-08 12:22:50 -0700

    level 1
    save

--- Player.gd
+++ Player.gd
@@ -50,13 +50,11 @@ var time_to_shoot = 0.5
 var Able_to_move = GameState.Able_to_move
 var Story_dialogue_finish = 1
 
-var is_platformer = GameState.platformer
+var is_platformer = false
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var gravity_scale = 1.5
-var jump_velocity = -1000
-var Double_jump = false
-
+var jump_velocity = -700
 
 func _ready():
 	TurnInventoryItemOff()
@@ -67,7 +65,6 @@ func _ready():
 	
 
 func _process(delta):
-	var is_platformer = GameState.platformer
 	var Able_to_move = GameState.Able_to_move
 	if InRangeWithEnemy == false:
 		killing_timer.stop()
@@ -324,4 +321,3 @@ func run_daiolge(number):
 	if number == 1:
 		DialogueManager.show_example_dialogue_balloon(load("res://Player.dialogue"),"Start")
 		
-
